3427e96d18d69cc7de966a7c2752e9671383eec5
Yolanda Diaz <yolicroni@gmail.com>
2023-01-08 19:15:33 +0100

    Adding info to readme

--- models/Anuncio.js
+++ models/Anuncio.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 
 
 const anuncioSchema = new mongoose.Schema({
-  name: { type: String, required: true, index: true},
-  sale: { type: Boolean, required: true, index: true},
-  price: { type: Number, required: true, min: 0, index: true},
-  photo: { type: String, required: true},
-  tags: { type: [String], required: true, index: true}
+  name: {type: String, required: true, index: true},
+  sale: {type: Boolean, required: true, index: true},
+  price: {type: Number, required: true, min: 0, index: true},
+  photo: {type: String, required: true},
+  tags: {type: [String], required: true, index: true}
 });
 
 anuncioSchema.statics.lista = function(filtro, skip, limit, fields, sort) {
@@ -18,7 +18,7 @@ anuncioSchema.statics.lista = function(filtro, skip, limit, fields, sort) {
   query.select(fields);
   query.sort(sort);
   return query.exec()
-}
+};
 
 const Anuncio = mongoose.model('Anuncio', anuncioSchema);
 
--- readme.md
+++ readme.md
@@ -29,3 +29,19 @@ npm run dev
 Adverts list:
 
 GET /api/anuncios
+
+Example to filter by price
+http://127.0.0.1:3000/api/anuncios?price=25
+
+Example to filter by sale
+http://127.0.0.1:3000/api/anuncios?sale=true
+
+Example to filter by field: name
+http://127.0.0.1:3000/api/anuncios?fields=name
+
+Pagination example to return ad 3 to 6:
+http://127.0.0.1:3000/api/anuncios?skip=2&limit=3
+
+The API returns the images of each advertisement by doing a
+request to url:
+http://127.0.0.1:3000/images/*.jpg
